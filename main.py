9e97955691167b0193c23199d597a110949719ea
puppylionmonkey <puppylionmonkey33@gmail.com>
2023-04-07 14:07:20 +0800

    判斷停發並截圖，計算數量 ok finish

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import os
 import time
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 
 from mobile_driver_config import driver, wait, actions
+from path_config import project_path
 from scroll_function import scroll_down_until_element_visible_top
+
+
+def count_files(path):
+    count = 0
+    for _, _, files in os.walk(path):
+        count += len(files)
+    return count
 
 
 def test_for_cathay():
@@ -36,11 +45,12 @@ def test_for_cathay():
     # 點擊卡片介紹
     driver.find_element(By.XPATH, credit_card_menu_xpath + " and contains(@class, 'mbOnly')]/parent::*//a[text()='卡片介紹']").click()
 
-    # 截圖
-    # 1. 找出所有anchor的id
+    # 停發卡截圖
+    # 找出所有anchor的id
     anchor_id_element_list = driver.find_elements(By.XPATH, "//*[contains(@class, 'anchor')]//*[contains(@data-anchor-btn, 'blockname')]")
     anchor_id_list = [anchor_id_element.get_attribute('data-anchor-btn') for anchor_id_element in anchor_id_element_list]
     ok_anchor_id_list = list()
+    # 跳過推薦卡片、熱門卡片
     for anchor_id in anchor_id_list:
         anchor_text = driver.find_element(By.XPATH, "//*[contains(@class, 'anchor')]//*[@data-anchor-btn='" + anchor_id + "']//p").text
         if anchor_text == '推薦卡片' or anchor_text == '熱門卡片':
@@ -48,8 +58,11 @@ def test_for_cathay():
         else:
             ok_anchor_id_list.append(anchor_id)
 
+    # 每個anchor點了後，滑到最上面讓slide顯示出來.
+    # 點每一個slide
+    # 確認當前title有沒有停發，如果有截圖
+    stop_card_count = 0
     for anchor_id in ok_anchor_id_list:
-        print(anchor_id)
         driver.find_element(By.XPATH, "//*[contains(@class, 'anchor')]//*[@data-anchor-btn='" + anchor_id + "']").click()
         time.sleep(2)  # todo: wait
         # locator = (By.XPATH, "//*[contains(@class, 'anchor')]//*[@data-anchor-btn='" + anchor_id + "' and contains(@class, 'active')]")
@@ -63,13 +76,19 @@ def test_for_cathay():
         time.sleep(1)
         # 如果底下有swiper，一個一個點
         swiper_element_list = driver.find_elements(By.XPATH, "//section[@data-anchor-block='" + anchor_id + "']//span[contains(@class, 'swiper-pagination-bullet')]")
-        # 到最前面
         if len(swiper_element_list) > 0:
             for swiper_element in swiper_element_list:
                 swiper_element.click()
-                time.sleep(0.5)
+                time.sleep(1)
                 # 一張卡一張卡點擊
-                print(driver.find_element(By.XPATH, section_xpath + "//*[@class='swiper-wrapper']//*[contains(@class, 'active')]//*[@class='cubre-m-compareCard__title']").text)
-        # 2. 每個tab點了後，滑到最上面.
-        # 3. 點每一個slide
-        # 4. 確認當前title有沒有停發，如果有截圖
+                card_name = driver.find_element(By.XPATH, section_xpath + "//*[@class='swiper-wrapper']//*[contains(@class, 'active')]//*[@class='cubre-m-compareCard__title']").text
+                if '停發' in card_name:
+                    time.sleep(1)
+                    driver.save_screenshot('停發卡截圖/' + card_name + '.jpg')
+                    stop_card_count += 1
+                    print(card_name)
+
+    # 確認截圖數量正確
+    print(stop_card_count)
+    print(count_files(project_path + '/停發卡截圖'))
+    assert stop_card_count == count_files(project_path + '/停發卡截圖')
